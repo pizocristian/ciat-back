72bba536d8490749113914e425047e961b4b4681
cristian <pizocristian@gmail.com>
2022-06-02 08:55:38 -0500

    nueva bd

--- src/main/resources/db/migration/V3__insertJWT.sql
+++ src/main/resources/db/migration/V3__insertJWT.sql
@@ -4,4 +4,4 @@ INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 
 INSERT INTO tb_plaga(nombre_plaga) VALUES('Sin plagas');
 INSERT INTO tb_plaga(nombre_plaga) VALUES('Escama roja');
-INSERT INTO tb_plaga(nombre_plaga) VALUES('Gualpa'); Dragonazul123*
+INSERT INTO tb_plaga(nombre_plaga) VALUES('Gualpa');
